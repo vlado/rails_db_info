29b553bb93935690f1b0d5190340d7cd1732e1cd
Vlado Cingel <vladocingel@gmail.com>
2014-01-06 23:14:32 +0100

    Use page links for pagination

--- app/helpers/rails_db_info/application_helper.rb
+++ app/helpers/rails_db_info/application_helper.rb
@@ -3,20 +3,51 @@ module RailsDbInfo
 
     def paginate_table_entries(entries)
       return if entries.total_pages == 1
-      prev_page_text = '< Previous page'
-      next_page_text = 'Next page >'
-      first_page     = 'First page..'
-      last_page      = '..Last page'
+      prev_page_text = '< Previous'
+      next_page_text = 'Next>'
 
       html = '<div class="pagination">'
-      html << (entries.first_page ? link_to(first_page, :page => entries.first_page) : "")
       html << (entries.previous_page ? link_to(prev_page_text, :page => entries.previous_page) : prev_page_text)
-      html << "&nbsp;Page #{entries.current_page} of #{entries.total_pages} &nbsp;"
+      html << "&nbsp;#{page_links_for_pagination(entries)}&nbsp;"
       html << (entries.next_page ? link_to(next_page_text, :page => entries.next_page) : next_page_text)
-      html << (entries.last_page ? link_to(last_page, :page => entries.last_page) : "")
       html << '</div>'
 
       sanitize(html)
+    end
+
+private
+
+    def page_links_for_pagination(entries)
+      pages = pages_for_pagination(entries)
+      links = []
+
+      pages.each_with_index do |page,index|
+        if page == entries.current_page
+          links << content_tag(:b, page)
+        else
+          links << link_to(page, :page => page)
+        end
+        links << " ... " if page != pages.last && (page + 1) != pages[index+1]
+      end
+
+      links.join(' ')
+    end
+
+    def pages_for_pagination(entries)
+      last_page = entries.total_pages
+      current_page = entries.current_page
+
+      pages = if last_page > 10
+        [1, 2, 3] +
+        (current_page-2..current_page+2).to_a +
+        (last_page-2..last_page).to_a
+      else
+        (1..last_page).to_a
+      end
+
+      pages = pages.uniq.select { |p| p > 0 && p <= last_page }
+
+      pages
     end
 
   end
