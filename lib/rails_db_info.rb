56becb7a1be3ad7f33b88219616b2d667239b860
Vlado Cingel <vladocingel@gmail.com>
2015-04-24 11:50:27 +0200

    Custom require relative that work with older rubies

--- lib/rails_db_info.rb
+++ lib/rails_db_info.rb
@@ -1,5 +1,12 @@
-require "rails_db_info/engine"
-require File.expand_path("rails/routes", File.dirname(__FILE__))
-
 module RailsDbInfo
+  # Custom require relative that work with older rubies also
+  def self.require_relative(path)
+    full_path = File.expand_path(path, File.dirname(__FILE__))
+    Kernel.require(full_path)
+  end
 end
+
+require "rails_db_info/engine"
+RailsDbInfo.require_relative("rails/routes")
+
+
